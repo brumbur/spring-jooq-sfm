b621a15a760763a65b77c8007642ab38d87e4017
brumbur <4alexander@gmail.com>
2021-09-27 19:56:28 -0400

    updated jooq configs

--- src/test/java/org/brum/sjs/app/AppTest.java
+++ src/test/java/org/brum/sjs/app/AppTest.java
@@ -1,22 +1,19 @@
 package org.brum.sjs.app;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @SpringBootTest(classes = App.class)
 @Transactional("transactionManager")
-@RunWith(SpringJUnit4ClassRunner.class)
 class AppTest {
 
     @Test
-    void main() {
+        void main() {
     }
 
     @Test
-    void commandLineRunner() {
+        void commandLineRunner() {
     }
 }
